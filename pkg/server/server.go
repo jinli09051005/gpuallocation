27e9c8c7c484841c1923f16f36a8f50afc2e7b76
jinli <jinli09051005@163.com>
2024-10-11 17:47:59 +0800

    add gpu cuda limit

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -3,8 +3,10 @@ package server
 import (
 	"context"
 	"fmt"
+	"os"
 	"strings"
 
+	"github.com/google/uuid"
 	"jinli.io/device-plugin/pkg/manager"
 	"k8s.io/klog/v2"
 	pluginapi "k8s.io/kubelet/pkg/apis/deviceplugin/v1beta1"
@@ -78,8 +80,10 @@ func (p *Plugin) ListAndWatch(e *pluginapi.Empty, s pluginapi.DevicePlugin_ListA
 }
 
 func (p *Plugin) Allocate(ctx context.Context, reqs *pluginapi.AllocateRequest) (*pluginapi.AllocateResponse, error) {
+	hostPath := "/usr/local/vgpu"
 	responses := pluginapi.AllocateResponse{}
 	for _, req := range reqs.ContainerRequests {
+		// req代表单个容器，要处理单个pod所有容器
 		response := pluginapi.ContainerAllocateResponse{
 			Envs: map[string]string{
 				"NVIDIA_VISIBLE_DEVICES": strings.Join(req.DevicesIDs, ","),
@@ -91,6 +95,44 @@ func (p *Plugin) Allocate(ctx context.Context, reqs *pluginapi.AllocateRequest) 
 				return nil, fmt.Errorf("error to get allocate response: unknown device: %s", id)
 			}
 		}
+
+		// gpu资源限制
+		for i := range req.DevicesIDs {
+			memKey := fmt.Sprintf("CUDA_DEVICE_MEMORY_LIMIT_%v", i)
+			response.Envs[memKey] = "20m"
+		}
+		//HAMI-core中CUDA_DEVICE_MEMORY_LIMIT_ID（限制容器指定设备显存）会覆盖CUDA_DEVICE_MEMORY_LIMIT（限制容器所有设备显存）
+		response.Envs["CUDA_DEVICE_MEMORY_LIMIT_0"] = "20m"
+		response.Envs["CUDA_DEVICE_SM_LIMIT"] = "50"
+		response.Envs["CUDA_DEVICE_MEMORY_SHARED_CACHE"] = fmt.Sprintf("%s/%v.cache", hostPath, uuid.New().String())
+		response.Envs["CUDA_OVERSUBSCRIBE"] = "true"
+
+		cacheFileHostDirectory := "/usr/local/vgpu/containers/poduid_containername"
+		os.RemoveAll(cacheFileHostDirectory)
+
+		os.MkdirAll(cacheFileHostDirectory, 0777)
+		os.Chmod(cacheFileHostDirectory, 0777)
+		os.MkdirAll("/tmp/vgpulock", 0777)
+		os.Chmod("/tmp/vgpulock", 0777)
+
+		response.Mounts = append(response.Mounts,
+			&pluginapi.Mount{
+				ContainerPath: fmt.Sprintf("%s/libvgpu.so", hostPath),
+				HostPath:      hostPath + "/libvgpu.so",
+				ReadOnly:      true},
+			&pluginapi.Mount{
+				ContainerPath: hostPath,
+				HostPath:      cacheFileHostDirectory,
+				ReadOnly:      false},
+			&pluginapi.Mount{
+				ContainerPath: "/tmp/vgpulock",
+				HostPath:      "/tmp/vgpulock",
+				ReadOnly:      false},
+			&pluginapi.Mount{
+				ContainerPath: "/etc/ld.so.preload",
+				HostPath:      hostPath + "/ld.so.preload",
+				ReadOnly:      true},
+		)
 
 		responses.ContainerResponses = append(responses.ContainerResponses, &response)
 	}
